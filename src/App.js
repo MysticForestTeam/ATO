caf6475216d50b54613ff2d5292c1b569c2ff72f
YoungProducer <37039996+YoungProducer@users.noreply.github.com>
2019-04-25 12:46:57 +0300

    Update structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { BrowserRouter, HashRouter, Route, Switch } from 'react-router-dom';
+import React from 'react'
+import { BrowserRouter, HashRouter, Route, Switch } from 'react-router-dom'
 
-import Home from './components/Home';
-import News from './components/News';
-import Gallery from './components/Gallery';
-import Contacts from './components/Contacts';
+import Home from './pages/Home'
+import News from './pages/News'
+import Gallery from './pages/Gallery'
+import Contacts from './pages/Contacts'
 
-import './css/normalize.css';
+import './css/normalize.css'
 
 class App extends React.Component {
   render() {
@@ -14,13 +14,13 @@ class App extends React.Component {
       <HashRouter>
         <Switch>
           <Route path='/' component={Home} exact />
-          <Route path='/news' component={News} />
+          {/* <Route path='/news' component={News} />
           <Route path='/gallery' component={Gallery} />
-          <Route path='/contacts' component={Contacts} />
+          <Route path='/contacts' component={Contacts} /> */}
         </Switch>
       </HashRouter>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import Header from '../modules/header'
+
+class Home extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+      </>
+    )
+  }
+}
+
+export default Home
