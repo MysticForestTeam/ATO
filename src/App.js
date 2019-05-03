d5cbec0b162a0f8aad836013c43ead62d0c67c4a
YoungCreator <sashabezrukovownmail@gmail.com>
2019-05-03 23:51:50 +0300

    Add lazy import

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense, lazy } from 'react';
 import { BrowserRouter, HashRouter, Route, Switch } from 'react-router-dom';
 import Header from './modules/header/Header';
 
-import Home from './pages/Home';
-import News from './pages/News';
-import Gallery from './pages/Gallery';
-import Contacts from './pages/Contacts';
-import Post from './pages/Post';
+const Home = lazy(() => import('./pages/Home'))
+const News = lazy(() => import('./pages/News'))
+const Gallery = lazy(() => import('./pages/Gallery'))
+const Contacts = lazy(() => import('./pages/Contacts'))
+const Post = lazy(() => import('./pages/Post'))
 
 import './css/normalize.css';
 
@@ -14,18 +14,20 @@ class App extends Component {
   render() {
     return (
       <HashRouter>
-        <Header />
-        <Switch>
-          <Route exact path='/' render={() => <Home />} />
-          <Route exact path='/news' render={props => <News {...props} />} />
-          <Route
-            exact
-            path='/news/:id'
-            render={params => <Post {...params} />}
-          />
-          <Route path='/gallery' render={() => <Gallery />} />
-          <Route path='/contacts' render={() => <Contacts />} />
-        </Switch>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Header />
+          <Switch>
+            <Route exact path='/' render={() => <Home />} />
+            <Route exact path='/news' render={props => <News {...props} />} />
+            <Route
+              exact
+              path='/news/:id'
+              render={params => <Post {...params} />}
+            />
+            <Route path='/gallery' render={() => <Gallery />} />
+            <Route path='/contacts' render={() => <Contacts />} />
+          </Switch>
+        </Suspense>
       </HashRouter>
     );
   }
