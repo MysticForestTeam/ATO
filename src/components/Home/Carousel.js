199f9c7532cd45d824f01f0ebeb5bb95c836dc8b
rainchuk <rainchuk.vitaliy@gmail.com>
2019-07-01 01:56:50 +0300

    Update Carousel.js

--- src/components/Home/Carousel.js
+++ src/components/Home/Carousel.js
@@ -1,126 +1,143 @@
-import React from 'react'
-import { Map, List } from 'immutable'
+import React from 'react';
+import { Map, List } from 'immutable';
 
 import {
-  Wrapper,
-  ImagesWrapper,
-  Image,
-  DotsWrapper,
-  Dot
-} from './Carousel.styled'
+	Wrapper,
+	ImagesWrapper,
+	Image,
+	DotsWrapper,
+	Dot
+} from './Carousel.styled';
 
 class Carousel extends React.Component {
-  constructor(props) {
-    super(props)
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      images: [],
-      activeDots: [],
-      translateX: 0,
-      activeSlide: 0,
-      animates: false,
-      url: '',
-      allowAnimation: true
-    }
+		this.mounted = false;
 
-    this.moveImages = this.moveImages.bind(this)
-    this.autoToggle = this.autoToggle.bind(this)
-    this.getUrl = this.getUrl.bind(this)
-  }
+		this.state = {
+			images: [],
+			activeDots: [],
+			translateX: 0,
+			activeSlide: 0,
+			animates: false,
+			url: '',
+			allowAnimation: true
+		};
 
-  getUrl = () => {
-  }
+		this.moveImages = this.moveImages.bind(this);
+		this.autoToggle = this.autoToggle.bind(this);
+		this.getUrl = this.getUrl.bind(this);
+	}
+	getUrl = () => {};
 
-  componentWillMount() {
-    this.getUrl()
-    window.setTimeout(() => {
-      if (window.location.pathname === this.props.workedLink) {
-        this.setState({ images: [...this.props.images, this.props.images[0]] })
-        this.setState({
-          activeDots: List.of(
-            ...this.props.images.map((image, index) => ({
-              id: index,
-              state: index === 0 ? true : false
-            }))
-            ).toJS()
-          })
-        }
-    }, 50)
-  }
-    
-  componentDidMount() {
-    window.setTimeout(() => {
-      if (window.location.pathname === this.props.workedLink) {
-        this.autoToggle()
-      }
-    }, 50)
-  }
+	componentWillMount() {
+		this.getUrl();
+		this.mounted = true;
+		this.timer1 = setTimeout(() => {
+			if (window.location.pathname === this.props.workedLink) {
+				if (this.mounted) {
+					this.setState({
+						images: [...this.props.images, this.props.images[0]]
+					});
+					this.setState({
+						activeDots: List.of(
+							...this.props.images.map((image, index) => ({
+								id: index,
+								state: index === 0 ? true : false
+							}))
+						).toJS()
+					});
+				}
+			}
+		}, 50);
+	}
 
-  moveImages = index => {
-    this.getUrl()
+	componentDidMount() {
+		this.mounted = true;
 
-    this.setState({
-      translateX:
-        this.state.translateX +
-        (232 + 20) *
-          ((index === this.state.activeDots.length ? 0 : index) -
-            this.state.activeSlide) *
-          -1,
-      activeSlide: index === this.state.activeDots.length ? 0 : index
-    })
+		this.timer2 = setTimeout(() => {
+			if (window.location.pathname === this.props.workedLink) {
+				this.autoToggle();
+			}
+		}, 50);
+	}
 
-    const activeDots = List.of(
-      ...this.state.activeDots.map(dot => {
-        return {
-          id: dot.id,
-          state:
-            dot.id === (index === this.state.activeDots.length ? 0 : index)
-              ? true
-              : false
-        }
-      })
-    )
+	componentWillUnmount() {
+		this.mounted = false;
+		clearTimeout(this.timer1);
+		clearTimeout(this.timer2);
+	}
 
-    this.setState({ activeDots: activeDots.toJS() })
-  }
+	moveImages = index => {
+		this.getUrl();
 
-  autoToggle = () => {
-    this.moveImages(this.state.activeSlide + 1)
-    
-    window.setTimeout(() => {
-      if (window.location.pathname === this.props.workedLink) {
-        this.autoToggle(this.state.activeSlide + 1)
-      }
-    }, this.props.delay)
-  }
+		if (this.mounted) {
+			this.setState({
+				translateX:
+					this.state.translateX +
+					(232 + 20) *
+						((index === this.state.activeDots.length ? 0 : index) -
+							this.state.activeSlide) *
+						-1,
+				activeSlide: index === this.state.activeDots.length ? 0 : index
+			});
+		}
 
-  render() {
-    return (
-      <Wrapper>
-        <ImagesWrapper>
-          {this.state.images.map((image, index) => (
-            <Image
-              key={index}
-              src={image}
-              transX={parseInt(this.state.translateX) + 'px'}
-            />
-          ))}
-        </ImagesWrapper>
-        <DotsWrapper>
-          {this.state.activeDots.map((dot, index) => (
-            <Dot
-              key={index}
-              active={dot}
-              onClick={() => {
-                this.moveImages(index)
-              }}
-              pose={dot.state ? 'active' : 'init'}
-            />
-          ))}
-        </DotsWrapper>
-      </Wrapper>
-    )
-  }
+		const activeDots = List.of(
+			...this.state.activeDots.map(dot => {
+				return {
+					id: dot.id,
+					state:
+						dot.id === (index === this.state.activeDots.length ? 0 : index)
+							? true
+							: false
+				};
+			})
+		);
+
+		if (this.mounted) {
+			this.setState({ activeDots: activeDots.toJS() });
+		}
+	};
+
+	autoToggle = () => {
+		this.moveImages(this.state.activeSlide + 1);
+
+		window.setTimeout(() => {
+			if (window.location.pathname === this.props.workedLink) {
+				this.autoToggle(this.state.activeSlide + 1);
+			}
+		}, this.props.delay);
+	};
+
+	render() {
+		return (
+			<Wrapper>
+				<ImagesWrapper>
+					{this.state.images.map((image, index) => (
+						<Image
+							key={index}
+							src={image}
+							transX={parseInt(this.state.translateX) + 'px'}
+						/>
+					))}
+				</ImagesWrapper>
+				<DotsWrapper>
+					{this.state.activeDots.map((dot, index) => (
+						<Dot
+							key={index}
+							active={dot}
+							onClick={() => {
+								this.moveImages(index);
+							}}
+							pose={dot.state ? 'active' : 'init'}
+						/>
+					))}
+				</DotsWrapper>
+			</Wrapper>
+		);
+	}
 }
 
-export default Carousel
+export default Carousel;
